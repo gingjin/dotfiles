4f231b87bdb8540aa17bc4b1b771ef5f999a299a
gingjin <gingjin@163.com>
2022-05-19 17:30:59 +0800

    update lazygit

--- dot_config/nvim/lua/config/lazygit.lua
+++ dot_config/nvim/lua/config/lazygit.lua
@@ -1,9 +1,8 @@
 -- nvim lazygit
-
+--
 local g = vim.g
 g.lazygit_floating_window_winblend = 0.85
 g.lazygit_floating_window_scaling_factor = 0.9
-g.lazygit_floating_window_corner_chars = { "╭", "╮", "╰", "╯" }
 g.lazygit_floating_window_use_plenary = 1
 g.lazygit_use_neovim_remote = 1
 
@@ -11,7 +10,3 @@ vim.cmd([[
 if has('nvim') && executable('nvr')
   let $GIT_EDITOR = "nvr -cc split --remote-wait +'set bufhidden=wipe'"
 endif]])
-
-local map = vim.api.nvim_set_keymap
-local opts = { noremap = true, silent = true }
-map("n", "<leader>gg", [[:LazyGit<cr>]], opts)
