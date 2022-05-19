f0412107f910c5ab5f49e6c173519aafed30c963
gingjin <gingjin@163.com>
2022-05-19 17:34:15 +0800

    update treesitter

--- dot_config/nvim/lua/config/treesitter.lua
+++ dot_config/nvim/lua/config/treesitter.lua
@@ -1,29 +1,25 @@
 -- nvim treesitter
 
-for _, config in pairs(require("nvim-treesitter.parsers").get_parser_configs()) do
-	config.install_info.url = config.install_info.url:gsub(
-		"https://github.com/",
-		"https://ghproxy.com/https://github.com/"
-	)
-end
+-- for _, config in pairs(require("nvim-treesitter.parsers").get_parser_configs()) do
+-- 	config.install_info.url = config.install_info.url:gsub(
+-- 		"https://github.com/",
+-- 		"https://ghproxy.com/https://github.com/"
+-- 	)
+-- end
 
 require("nvim-treesitter.configs").setup({
 	ensure_installed = "all",
-	ignore_install = { "swift" },
+	ignore_install = {},
 	sync_install = false,
 	highlight = {
 		enable = true,
-		additional_vim_regex_highlighting = true,
+		additional_vim_regex_highlighting = false,
 	},
 	indent = { enable = true },
-	autotag = { enable = true },
 	rainbow = {
 		enable = true,
 		extended_mode = true,
 		max_file_lines = nil,
-	},
-	context_commentstring = {
-		enable = true,
 	},
 	incremental_selection = {
 		enable = true,
@@ -33,5 +29,30 @@ require("nvim-treesitter.configs").setup({
 			node_decremental = "<BS>",
 			scope_incremental = "<TAB>",
 		},
+	},
+	autotag = { enable = true },
+	context_commentstring = { enable = true },
+	playground = {
+		enable = true,
+		disable = {},
+		updatetime = 25,
+		persist_queries = false,
+		keybindings = {
+			toggle_query_editor = "o",
+			toggle_hl_groups = "i",
+			toggle_injected_languages = "t",
+			toggle_anonymous_nodes = "a",
+			toggle_language_display = "I",
+			focus_language = "f",
+			unfocus_language = "F",
+			update = "R",
+			goto_node = "<cr>",
+			show_help = "?",
+		},
+	},
+	query_linter = {
+		enable = true,
+		use_virtual_text = true,
+		lint_events = { "BufWrite", "CursorHold" },
 	},
 })
