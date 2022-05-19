ba30afbc2cf59f3299e78632b89ea2ff2c9018dc
gingjin <gingjin@163.com>
2022-05-19 17:28:19 +0800

    add autocompletion

---
+++ dot_config/nvim/lua/config/autocompletion.lua
@@ -0,0 +1,91 @@
+-- nvim cmp
+--
+local cmp = require("cmp")
+cmp.setup({
+	sorting = {
+		comparators = {
+			cmp.config.compare.offset,
+			cmp.config.compare.exact,
+			cmp.config.compare.score,
+			require("cmp-under-comparator").under,
+			cmp.config.compare.kind,
+			cmp.config.compare.sort_text,
+			cmp.config.compare.length,
+			cmp.config.compare.order,
+		},
+	},
+	snippet = {
+		expand = function(args)
+			require("luasnip").lsp_expand(args.body)
+		end,
+	},
+	enabled = function()
+		return vim.api.nvim_buf_get_option(0, "buftype") ~= "prompt" or require("cmp_dap").is_dap_buffer()
+	end,
+	sources = cmp.config.sources({
+		{ name = "dap" },
+		{ name = "calc" },
+		{ name = "path" },
+		{ name = "buffer" },
+		{ name = "luasnip" },
+		{ name = "nvim_lua" },
+		{ name = "nvim_lsp" },
+		{ name = "treesitter" },
+		{ name = "nvim_lsp_signature_help" },
+		{ name = "npm", keyword_length = 4 },
+		-- { name = "vim-dadbod-completion" },
+	}),
+	mapping = cmp.mapping.preset.insert(require("keymaps").cmp(cmp)),
+	formatting = {
+		format = function(_, vim_item)
+			vim_item.kind = ({
+				Text = "",
+				Method = "",
+				Function = "",
+				Constructor = "",
+				Field = "",
+				Variable = "",
+				Class = "𝓒",
+				Interface = "ﰮ",
+				Module = "",
+				Property = "",
+				Unit = "",
+				Value = "",
+				Enum = "",
+				Keyword = "",
+				Snippet = "",
+				Color = "",
+				File = "",
+				Reference = "",
+				Folder = "",
+				EnumMember = "",
+				Constant = "",
+				Struct = "𝓢",
+				Event = "",
+				Operator = "ﬦ",
+				TypeParameter = "𝙏",
+			})[vim_item.kind]
+			return vim_item
+		end,
+	},
+})
+
+local sources = { "/", "?" }
+for _, source in pairs(sources) do
+	cmp.setup.cmdline(source, {
+		mapping = cmp.mapping.preset.cmdline(),
+		sources = cmp.config.sources({
+			{ name = "nvim_lsp_document_symbol" },
+			{ name = "buffer" },
+		}),
+	})
+end
+
+cmp.setup.cmdline(":", {
+	mapping = cmp.mapping.preset.cmdline(),
+	sources = cmp.config.sources({
+		{ name = "cmdline" },
+		{ name = "path" },
+		{ name = "buffer" },
+	}),
+})
