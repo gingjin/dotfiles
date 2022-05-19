7ffe44fab224602debf733a8c6c53d626c81dfa7
gingjin <gingjin@163.com>
2022-05-19 17:31:36 +0800

    update markdown-preview

--- dot_config/nvim/lua/config/markdown-preview.lua
+++ dot_config/nvim/lua/config/markdown-preview.lua
@@ -5,6 +5,3 @@ g.mkdp_auto_start = 0
 g.mkdp_auto_close = 1
 g.mkdp_refresh_slow = 1
 g.mkdp_page_title = "『${name}』"
-
-local map = vim.api.nvim_set_keymap
-map("n", "<leader>mp", [[<Plug>MarkdownPreviewToggle]], {})
