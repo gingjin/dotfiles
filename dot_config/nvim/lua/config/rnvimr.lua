d82d4c731533c342b83faed3808295e158abfa3c
gingjin <gingjin@163.com>
2022-05-19 17:32:35 +0800

    update rnvimr

--- dot_config/nvim/lua/config/rnvimr.lua
+++ dot_config/nvim/lua/config/rnvimr.lua
@@ -1,16 +1,10 @@
 -- nvim ranger
 
 local g = vim.g
-g.rnvimr_enable_ex = 1
+g.rnvimr_enable_ex = 0
 g.rnvimr_enable_picker = 1
 g.rnvimr_hide_gitignore = 1
 g.rnvimr_border_attr = { fg = 14, bg = -1 }
 g.rnvimr_enable_bw = 1
 
 vim.cmd("highlight link RnvimrNormal CursorLine")
-
-local map = vim.api.nvim_set_keymap
-local opts = { noremap = true, silent = true }
-map("t", "<A-i>", [[<C-\><C-n>:RnvimrResize<CR>]], opts)
-map("n", "<A-r>", [[:RnvimrToggle<CR>]], opts)
-map("t", "<A-r>", [[<C-\><C-n>:RnvimrToggle<CR>]], opts)
