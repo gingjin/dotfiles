ede58df4357919c3756afea111a7e2e088f6f64d
gingjin <gingjin@163.com>
2022-05-19 17:29:42 +0800

    update dap

--- dot_config/nvim/lua/config/dap.lua
+++ dot_config/nvim/lua/config/dap.lua
@@ -1,78 +1,17 @@
 -- nvim dap
-
+--
 local dap = require("dap")
-local dapui = require("dapui")
-dap.listeners.after.event_initialized["dapui_config"] = function()
-	dapui.open()
-end
-dap.listeners.before.event_terminated["dapui_config"] = function()
-	dapui.close()
-end
-dap.listeners.before.event_exited["dapui_config"] = function()
-	dapui.close()
-end
-
-dapui.setup({
-	icons = { expanded = "", collapsed = "" },
-	mappings = {
-		expand = { "<CR>", "<2-LeftMouse>" },
-		open = "o",
-		remove = "d",
-		edit = "e",
-		repl = "r",
-		toggle = "t",
-	},
-	sidebar = {
-		elements = {
-			{ id = "scopes", size = 0.25 },
-			{ id = "breakpoints", size = 0.25 },
-			{ id = "stacks", size = 0.25 },
-			{ id = "watches", size = 0.25 },
-		},
-		size = 30,
-		position = "left",
-	},
-	tray = {
-		elements = { "repl" },
-		size = 10,
-		position = "bottom",
-	},
-	floating = {
-		max_height = nil,
-		max_width = nil,
-		border = "double",
-		mappings = {
-			close = { "q", "<Esc>" },
-		},
-	},
-	windows = { indent = 1 },
-	render = {
-		max_type_length = nil,
-	},
-})
-
-require("nvim-dap-virtual-text").setup({
-	enabled = true,
-	enabled_commands = true,
-	highlight_changed_variables = true,
-	highlight_new_as_changed = false,
-	show_stop_reason = false,
-	commented = false,
-	virt_text_pos = "eol",
-	all_frames = true,
-	virt_lines = false,
-	virt_text_win_col = nil,
-})
-
 dap.defaults.fallback.external_terminal = true
 dap.defaults.fallback.focus_terminal = true
-vim.cmd([[au FileType dap-repl lua require('dap.ext.autocompl').attach()]])
 
-vim.fn.sign_define("DapBreakpoint", { text = "", texthl = "", numhl = "" })
-vim.fn.sign_define("DapBreakpointCondition", { text = "", texthl = "", numhl = "" })
-vim.fn.sign_define("DapLogPoint", { text = "", texthl = "", numhl = "" })
-vim.fn.sign_define("DapStopped", { text = "", texthl = "", numhl = "" })
-vim.fn.sign_define("DapBreakpointRejected", { text = "", texthl = "", numhl = "" })
+vim.cmd("au FileType dap-repl lua require('dap.ext.autocompl').attach()")
+
+local sign = vim.fn.sign_define
+sign("DapBreakpoint", { text = "", texthl = "", numhl = "" })
+sign("DapBreakpointCondition", { text = "", texthl = "", numhl = "" })
+sign("DapLogPoint", { text = "", texthl = "", numhl = "" })
+sign("DapStopped", { text = "", texthl = "", numhl = "" })
+sign("DapBreakpointRejected", { text = "", texthl = "", numhl = "" })
 
 -- c/c++/rust
 dap.adapters.cppdbg = {
@@ -85,7 +24,7 @@ dap.configurations.cpp = {
 		name = "Launch file",
 		type = "cppdbg",
 		request = "launch",
-		miDebuggerPath = "/usr/bin/gdb",
+		miDebuggerPath = "/usr/bin/gdb", -- Adjust to where gdb is installed
 		program = function()
 			return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
 		end,
@@ -151,16 +90,16 @@ dap.configurations.python = {
 		request = "launch",
 		name = "Launch file",
 		program = "${file}",
-		-- pythonPath = function()
-		-- 	local cwd = vim.fn.getcwd()
-		-- 	if vim.fn.executable(cwd .. "/venv/bin/python") == 1 then
-		-- 		return cwd .. "/venv/bin/python"
-		-- 	elseif vim.fn.executable(cwd .. "/.venv/bin/python") == 1 then
-		-- 		return cwd .. "/.venv/bin/python"
-		-- 	else
-		-- 		return "/usr/bin/python"
-		-- 	end
-		-- end,
+		pythonPath = function()
+			local cwd = vim.fn.getcwd()
+			if vim.fn.executable(cwd .. "/venv/bin/python") == 1 then
+				return cwd .. "/venv/bin/python"
+			elseif vim.fn.executable(cwd .. "/.venv/bin/python") == 1 then
+				return cwd .. "/.venv/bin/python"
+			else
+				return "/usr/bin/python"
+			end
+		end,
 	},
 }
 
@@ -176,7 +115,7 @@ dap.listeners.after["event_initialized"]["me"] = function()
 			end
 		end
 	end
-	api.nvim_set_keymap("n", "<M-K>", ':lua require("dap.ui.widgets").hover()<CR>', { silent = true })
+	api.nvim_set_keymap("n", "<M-K>", ':lua require("dap.ui.widgets").hover()<cr>', { silent = true })
 end
 
 dap.listeners.after["event_terminated"]["me"] = function()
@@ -186,14 +125,72 @@ dap.listeners.after["event_terminated"]["me"] = function()
 	keymap_restore = {}
 end
 
-local map = vim.api.nvim_set_keymap
-local opts = { noremap = true, silent = true }
-map("n", "<F5>", [[:lua require'dap'.continue()<cr>]], opts)
-map("n", "<F6>", [[:lua require'dap'.step_over()<cr>]], opts)
-map("n", "<F7>", [[:lua require'dap'.step_into()<cr>]], opts)
-map("n", "<F8>", [[:lua require'dap'.step_out()<cr>]], opts)
-map("n", "<leader>b", [[:lua require'dap'.toggle_breakpoint()<cr>]], opts)
-map("n", "<leader>B", [[:lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<cr>]], opts)
-map("n", "<leader>lp", [[:lua require'dap'.set_breakpoint(nil, nil, vim.fn.input('Log point message: '))<cr>]], opts)
-map("n", "<leader>dr", [[:lua require'dap'.repl.toggle()<cr>]], opts)
-map("n", "<leader>dl", [[:lua require'dap'.run_last()<cr>]], opts)
+-- nvim dapui
+--
+local dapui = require("dapui")
+dapui.setup({
+	icons = { expanded = "", collapsed = "" },
+	mappings = {
+		expand = { "<CR>", "<2-LeftMouse>" },
+		open = "o",
+		remove = "d",
+		edit = "e",
+		repl = "r",
+		toggle = "t",
+	},
+	sidebar = {
+		elements = {
+			{ id = "scopes", size = 0.25 },
+			{ id = "breakpoints", size = 0.25 },
+			{ id = "stacks", size = 0.25 },
+			{ id = "watches", size = 0.25 },
+		},
+		size = 30,
+		position = "left",
+	},
+	tray = {
+		elements = { "repl" },
+		size = 10,
+		position = "bottom",
+	},
+	floating = {
+		max_height = nil,
+		max_width = nil,
+		border = "double",
+		mappings = {
+			close = { "q", "<Esc>" },
+		},
+	},
+	windows = { indent = 1 },
+	render = {
+		max_type_length = nil,
+	},
+})
+
+dap.listeners.after.event_initialized["dapui_config"] = function()
+	dapui.open()
+end
+dap.listeners.before.event_terminated["dapui_config"] = function()
+	dapui.close()
+end
+dap.listeners.before.event_exited["dapui_config"] = function()
+	dapui.close()
+end
+
+-- nvim-dap-virtual-text
+--
+require("nvim-dap-virtual-text").setup({
+	enabled = true,
+	enabled_commands = true,
+	highlight_changed_variables = true,
+	highlight_new_as_changed = false,
+	show_stop_reason = false,
+	commented = false,
+	only_first_definition = true,
+	all_references = false,
+	filter_references_pattern = "<module",
+	virt_text_pos = "eol",
+	all_frames = false,
+	virt_lines = false,
+	virt_text_win_col = nil,
+})
