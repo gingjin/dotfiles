11b3ed31858ae82ba3d9a5e0c8a142a4cbed2f58
gingjin <gingjin@163.com>
2022-05-19 17:28:46 +0800

    update colorizer

---
+++ dot_config/nvim/lua/config/colorizer.lua
@@ -0,0 +1,15 @@
+-- nvim colorizer
+--
+require("colorizer").setup({
+	default_options = {
+		RGB = true,
+		RRGGBB = true,
+		names = true,
+		RRGGBBAA = true,
+		rgb_fn = true,
+		hsl_fn = true,
+		css = true,
+		css_fn = true,
+		mode = "background",
+	},
+})
