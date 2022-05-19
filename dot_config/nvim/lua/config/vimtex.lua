be966d8def2f1177015d805e3346276001e4d50c
gingjin <gingjin@163.com>
2022-05-19 17:36:46 +0800

    update vimtex

--- dot_config/nvim/lua/config/vimtex.lua
+++ dot_config/nvim/lua/config/vimtex.lua
@@ -2,7 +2,7 @@
 
 local g = vim.g
 g.tex_flavor = "latex"
-g.tex_conceal = "abdmg"
+-- g.tex_conceal = "abdmg"
 g.vimtex_quickfix_mode = 0
 g.vimtex_quickfix_open_on_warning = 0
 g.vimtex_view_method = "zathura"
@@ -17,6 +17,17 @@ g.vimtex_compiler_latexmk_engines = {
 	_ = "-xelatex",
 }
 
-local map = vim.api.nvim_set_keymap
-local opts = { noremap = true }
-map("n", "<leader>lw", [[:VimtexCountWords<cr>]], opts)
+local cmd = vim.api.nvim_create_autocmd
+local augroup = vim.api.nvim_create_augroup
+local vimtex_config = augroup("vimtex_config", { clear = true })
+cmd("User", {
+	group = vimtex_config,
+	pattern = "VimtexEventQuit",
+	command = "call vimtex#compiler#clean(0)",
+})
+local VimTeX = augroup("VimTeX", { clear = true })
+cmd("BufWritePost", {
+  group = VimTeX,
+  pattern = "*.tex",
+  command = "call vimtex#toc#refresh()",
+})
