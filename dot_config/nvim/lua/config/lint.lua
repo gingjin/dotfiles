5ec181749d304767a73cf68abb26f74d66d2f5b7
gingjin <gingjin@163.com>
2022-05-19 17:31:08 +0800

    add lint

---
+++ dot_config/nvim/lua/config/lint.lua
@@ -0,0 +1,9 @@
+-- nvim lint
+
+require("lint").linters_by_ft = {
+	cpp = { "cpplint" },
+	python = { "flake8" },
+}
+
+vim.cmd("au BufEnter * lua require('lint').try_lint()")
+vim.cmd("au BufWritePost <buffer> lua require('lint').try_lint()")
