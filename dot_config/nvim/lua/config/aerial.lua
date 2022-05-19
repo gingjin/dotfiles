1583c7f841b744dcc929008c3d4d500cf6627ff8
gingjin <gingjin@163.com>
2022-05-19 17:27:50 +0800

    update aerial

--- dot_config/nvim/lua/config/aerial.lua
+++ dot_config/nvim/lua/config/aerial.lua
@@ -1,5 +1,5 @@
 -- nvim aerial
-
+--
 require("aerial").setup({
 	lsp = {
 		diagnostics_trigger_update = true,
@@ -9,24 +9,19 @@ require("aerial").setup({
 	treesitter = { update_delay = 300 },
 	filter_kind = false,
 	on_attach = function(bufnr)
-		local map = vim.api.nvim_buf_set_keymap
-		map(bufnr, "n", "<leader>at", [[:AerialToggle!<cr>]], {})
-		map(bufnr, "n", "{", [[:AerialPrevUp<cr>]], {})
-		map(bufnr, "n", "}", [[:AerialNextUp<cr>]], {})
-		map(bufnr, "n", "[[", [[:AerialPrev<cr>]], {})
-		map(bufnr, "n", "]]", [[:AerialNext<cr>]], {})
+    local function bufmap(...)
+      vim.api.nvim_buf_set_keymap(bufnr, ...)
+    end
+    require("keymaps").aerial(bufmap)
 	end,
 	close_behavior = "close",
 	nerd_font = true,
 	highlight_on_hover = true,
 })
 
-vim.cmd([[hi link AerialClass Type]])
-vim.cmd([[hi link AerialClassIcon Special]])
-vim.cmd([[hi link AerialFunction Special]])
-vim.cmd([[hi AerialFunctionIcon guifg=#cb4b16 guibg=NONE guisp=NONE gui=NONE cterm=NONE]])
-vim.cmd([[hi link AerialGuide Comment]])
-
-local map = vim.api.nvim_set_keymap
-local opts = { noremap = true, silent = true }
-map("n", "<leader>at", [[:AerialToggle<cr>]], opts)
+local cmd = vim.cmd
+cmd("hi link AerialClass Type")
+cmd("hi link AerialClassIcon Special")
+cmd("hi link AerialFunction Special")
+cmd("hi AerialFunctionIcon guifg=#cb4b16 guibg=NONE guisp=NONE gui=NONE cterm=NONE")
+cmd("hi link AerialGuide Comment")
