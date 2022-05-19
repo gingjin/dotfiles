b349d2ac4ac602b8c8ed578296feba395a00e195
gingjin <gingjin@163.com>
2022-05-19 17:32:02 +0800

    update neoformat

--- dot_config/nvim/lua/config/neoformat.lua
+++ dot_config/nvim/lua/config/neoformat.lua
@@ -1,17 +1,16 @@
 -- nvim neoformat
 
--- local cmd = vim.api.nvim_command
--- cmd("augroup fmt")
--- cmd("autocmd!")
--- cmd("autocmd BufWritePre * undojoin | Neoformat")
--- cmd("augroup end")
+-- local augroup = vim.api.nvim_create_augroup
+-- local cmd = vim.api.nvim_create_autocmd
+-- local formatOnSave = augroup("fmt", { clear = true })
+-- cmd("BufWritePre", {
+-- 	group = formatOnSave,
+-- 	pattern = "*",
+-- 	command = "undojoin | Neoformat",
+-- })
 
 local g = vim.g
 g.neoformat_basic_format_trim = 1
 g.neoformat_basic_format_retab = 1
 g.neoformat_basic_format_align = 1
 g.neoformat_run_all_formatters = 1
-
-local map = vim.api.nvim_set_keymap
-local opts = { noremap = true, silent = true }
-map("n", "<leader>i", [[:Neoformat<cr>]], opts)
