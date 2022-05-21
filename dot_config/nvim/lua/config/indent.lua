a8c4b61dc1a9baf683b59614cc121929dfaaa461
gingjin <gingjin@163.com>
2022-05-21 16:25:10 +0800

    update indent

--- dot_config/nvim/lua/config/indent.lua
+++ dot_config/nvim/lua/config/indent.lua
@@ -2,8 +2,6 @@
 --
 require("indent_blankline").setup({
   char = "⎸",
-	show_current_context = true,
-	show_current_context_start = false,
 	show_first_indent_level = true,
 	show_trailing_blankline_indent = false,
 })
