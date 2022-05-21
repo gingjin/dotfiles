ccc21e721f468412b466f809a274746c98711d70
gingjin <gingjin@163.com>
2022-05-21 15:38:41 +0800

    add dapui

---
+++ dot_config/nvim/lua/config/dapui.lua
@@ -0,0 +1,52 @@
+-- nvim dapui
+--
+local dapui = require("dapui")
+dapui.setup({
+	icons = { expanded = "", collapsed = "" },
+	mappings = {
+		expand = { "<CR>", "<2-LeftMouse>" },
+		open = "o",
+		remove = "d",
+		edit = "e",
+		repl = "r",
+		toggle = "t",
+	},
+	sidebar = {
+		elements = {
+			{ id = "scopes", size = 0.25 },
+			{ id = "breakpoints", size = 0.25 },
+			{ id = "stacks", size = 0.25 },
+			{ id = "watches", size = 0.25 },
+		},
+		size = 30,
+		position = "left",
+	},
+	tray = {
+		elements = { "repl" },
+		size = 10,
+		position = "bottom",
+	},
+	floating = {
+		max_height = nil,
+		max_width = nil,
+		border = "double",
+		mappings = {
+			close = { "q", "<Esc>" },
+		},
+	},
+	windows = { indent = 1 },
+	render = {
+		max_type_length = nil,
+	},
+})
+
+local dap = require("dap")
+dap.listeners.after.event_initialized["dapui_config"] = function()
+	dapui.open()
+end
+dap.listeners.before.event_terminated["dapui_config"] = function()
+	dapui.close()
+end
+dap.listeners.before.event_exited["dapui_config"] = function()
+	dapui.close()
+end
