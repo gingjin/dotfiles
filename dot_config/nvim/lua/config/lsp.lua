1c88b6e76ad24a9f75f51b3912c8fe398146704f
gingjin <gingjin@163.com>
2022-05-21 15:43:21 +0800

    update lsp

--- dot_config/nvim/lua/config/lsp.lua
+++ dot_config/nvim/lua/config/lsp.lua
@@ -1,43 +1,5 @@
 -- nvim lspconfig
 --
-local servers = {
-	"bashls",
-	"clangd",
-	"cssls",
-	"gopls",
-	"html",
-	"intelephense",
-	"jdtls",
-	"jsonls",
-	"julials",
-	"lemminx",
-	"ltex",
-	"pyright",
-	"r_language_server",
-	"rust_analyzer",
-	"serve_d",
-	"sorbet",
-	"sqlls",
-	"sumneko_lua",
-	"tsserver",
-	"vimls",
-	"vuels",
-	"yamlls",
-}
-
--- nvim-lsp-installer
-require("nvim-lsp-installer").setup({
-	ensure_installed = servers,
-	automatic_installation = true,
-	ui = {
-		icons = {
-			server_installed = "",
-			server_pending = "",
-			server_uninstalled = "",
-		},
-	},
-})
-
 local on_attach = function(_, bufnr)
 	local function bufmap(...)
 		vim.api.nvim_buf_set_keymap(bufnr, ...)
@@ -54,7 +16,7 @@ local handlers = {
 
 local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
 
-for _, server in ipairs(servers) do
+for _, server in ipairs(require("options").servers()) do
 	if server == "sumneko_lua" then
 		local runtime_path = vim.split(package.path, ";")
 		table.insert(runtime_path, "nvim/lua/?/?.lua")
@@ -79,57 +41,3 @@ for _, server in ipairs(servers) do
 		capabilities = capabilities,
 	})
 end
-
--- nvim lspsaga
---
-require("lspsaga").setup({
-	debug = false,
-	use_saga_diagnostic_sign = true,
-	error_sign = "",
-	warn_sign = "",
-	infor_sign = "",
-	hint_sign = "",
-	diagnostic_header_icon = " ",
-	code_action_icon = " ",
-	code_action_prompt = {
-		enable = true,
-		sign = true,
-		sign_priority = 40,
-		virtual_text = false,
-	},
-	finder_definition_icon = "ﳗ ",
-	finder_reference_icon = " ",
-	max_preview_lines = 10,
-	finder_action_keys = {
-		open = "o",
-		vsplit = "s",
-		split = "i",
-		quit = "q",
-		scroll_down = "<C-d>",
-		scroll_up = "<C-u>",
-	},
-	code_action_keys = { quit = "q", exec = "<CR>" },
-	rename_action_keys = { quit = "<C-c>", exec = "<CR>" },
-	definition_preview_icon = " ",
-	border_style = "double",
-	rename_prompt_prefix = "",
-	rename_output_qflist = {
-		enable = false,
-		auto_open_qflist = true,
-	},
-	server_filetype_map = {},
-	diagnostic_prefix_format = "%d. ",
-	diagnostic_message_format = "%m %c",
-	highlight_prefix = true,
-})
-
-vim.cmd("highlight link LspSagaFinderSelection Search")
-
-local cmd = vim.api.nvim_create_autocmd
-local augroup = vim.api.nvim_create_augroup
-local lspsaga_filetypes = augroup("lspsaga_filetypes", { clear = true })
-cmd("FileType", {
-	group = lspsaga_filetypes,
-	pattern = "LspsagaHover",
-	command = "nnoremap <buffer><nowait><silent> <Esc> <cmd>close!<cr>",
-})
