6e70ce7542175af5985b74665baf6756060e1a40
gingjin <gingjin@163.com>
2022-05-22 13:14:10 +0800

    update treesitter

--- dot_config/nvim/lua/config/treesitter.lua
+++ dot_config/nvim/lua/config/treesitter.lua
@@ -1,13 +1,5 @@
 -- nvim treesitter
 --
-
--- for _, config in pairs(require("nvim-treesitter.parsers").get_parser_configs()) do
--- 	config.install_info.url = config.install_info.url:gsub(
--- 		"https://github.com/",
--- 		"https://ghproxy.com/https://github.com/"
--- 	)
--- end
-
 require("nvim-treesitter.configs").setup({
 	ensure_installed = "all",
 	ignore_install = {},
@@ -52,28 +44,5 @@ require("nvim-treesitter.configs").setup({
 				},
 			},
 		},
-	},
-	playground = {
-		enable = true,
-		disable = {},
-		updatetime = 25,
-		persist_queries = false,
-		keybindings = {
-			toggle_query_editor = "o",
-			toggle_hl_groups = "i",
-			toggle_injected_languages = "t",
-			toggle_anonymous_nodes = "a",
-			toggle_language_display = "I",
-			focus_language = "f",
-			unfocus_language = "F",
-			update = "R",
-			goto_node = "<cr>",
-			show_help = "?",
-		},
-	},
-	query_linter = {
-		enable = true,
-		use_virtual_text = true,
-		lint_events = { "BufWrite", "CursorHold" },
 	},
 })
