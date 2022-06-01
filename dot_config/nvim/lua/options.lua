939376be0a6e50747bd8f4db00a258764b3cd15a
gingjin <gingjin@163.com>
2022-06-01 15:54:07 +0800

    update options.lua

--- dot_config/nvim/lua/options.lua
+++ dot_config/nvim/lua/options.lua
@@ -1,82 +1,36 @@
+local cmd = vim.cmd
+cmd("filetype plugin indent on")
+
 local g = vim.g
 g.mapleader = " "
 g.maplocalleader = " "
-g.python3_host_prog = "~/.pyenv/versions/py3.7/bin/python3.7"
-
-local cmd = vim.cmd
-cmd("filetype plugin on")
-cmd("filetype indent on")
-
-local options = {
-	autowrite = true,
-	autowriteall = true,
-	backup = false,
-	clipboard = "unnamedplus",
-	cmdheight = 2,
-	completeopt = { "menu", "menuone", "noselect" },
-	-- cursorcolumn = true,
-	cursorline = true,
-	encoding = "utf-8",
-	expandtab = true,
-	fileencodings = "ucs-bom,utf-8,gbk",
-	foldenable = false,
-	foldexpr = "nvim_treesitter#foldexpr()",
-	foldlevel = 40,
-	foldmethod = "expr",
-	ignorecase = true,
-	incsearch = true,
-	linebreak = true,
-	mouse = "a",
-	number = true,
-	relativenumber = true,
-	scrolloff = 8,
-	shiftround = true,
-	shiftwidth = 2,
-	showmode = false,
-	sidescrolloff = 8,
-	signcolumn = "yes",
-	smartcase = true,
-	smartindent = true,
-	softtabstop = 2,
-	splitbelow = true,
-	splitright = true,
-	swapfile = false,
-	tabstop = 2,
-	termguicolors = true,
-	undofile = true,
-	wrap = true,
-	writebackup = false,
-}
+g.python3_host_prog = "~/.pyenv/versions/py3.7/bin/python"
 
 local opt = vim.opt
-for k, v in pairs(options) do
-	opt[k] = v
-end
-
-local M = {}
-M.servers = function()
-	return {
-		"bashls",
-		"clangd",
-		"cssls",
-		"gopls",
-		"html",
-		"intelephense",
-		"jdtls",
-		"jsonls",
-		"julials",
-		"lemminx",
-		"ltex",
-		"pyright",
-		"r_language_server",
-		"rust_analyzer",
-		"sorbet",
-		"sqlls",
-		"sumneko_lua",
-		"tsserver",
-		"vimls",
-		"vuels",
-		"yamlls",
-	}
-end
-return M
+opt.autoread = true
+opt.autowrite = true
+opt.clipboard = "unnamedplus"
+opt.completeopt = "menu,menuone,noselect"
+opt.cursorline = true
+opt.encoding = "UTF-8"
+opt.expandtab = true
+opt.fileencodings = "ucs-bom,utf-8,gbk"
+opt.foldenable = false
+opt.foldexpr = "nvim_treesitter#foldexpr()"
+opt.foldlevel = 99
+opt.foldmethod = "expr"
+opt.ignorecase = true
+opt.mouse = "a"
+opt.number = true
+opt.relativenumber = true
+opt.scrolloff = 8
+opt.shiftround = true
+opt.shiftwidth = 2
+opt.signcolumn = "yes"
+opt.smartcase = true
+opt.smartindent = true
+opt.softtabstop = 2
+opt.splitbelow = true
+opt.splitright = true
+opt.tabstop = 2
+opt.termguicolors = true
