5cb51845a6dd30277cd4c804c86a8bf606e0e924
gingjin <gingjin@163.com>
2022-06-01 15:50:48 +0800

    update telescope.lua

--- dot_config/nvim/lua/conf/telescope.lua
+++ dot_config/nvim/lua/conf/telescope.lua
@@ -2,36 +2,39 @@
 --
 local tele = require("telescope")
 tele.setup({
-	defaults = {
-		multi_icon = "",
-		prompt_prefix = " ",
-		selection_caret = " ",
-		width = 0.8,
-		previewer = true,
-		prompt_title = true,
-		file_ignore_patterns = { "__pycache__", ".git" },
-	},
-	extensions = {
-		fzf = {
-			fuzzy = true,
-			override_generic_sorter = true,
-			override_file_sorter = true,
-			case_mode = "smart_case",
-		},
-		bookmarks = {
-			selected_browser = "chrome",
-			url_open_command = "open",
-			url_open_plugin = "open_browser",
-			full_path = true,
-			firefox_profile_name = nil,
-		},
-		symbols = {
-			sources = { "emoji", "kaomoji", "gitmoji", "latex", "math" },
-		},
-	},
+  defaults = {
+    multi_icon = "",
+    prompt_prefix = " ",
+    selection_caret = " ",
+    width = 0.8,
+    previewer = true,
+    prompt_title = true,
+    file_ignore_patterns = { "__pycache__", ".git" },
+  },
+  extensions = {
+    fzf = {
+      fuzzy = true,
+      override_generic_sorter = true,
+      override_file_sorter = true,
+      case_mode = "smart_case",
+    },
+    bookmarks = {
+      selected_browser = "chrome",
+      url_open_command = "open",
+      url_open_plugin = "open_browser",
+      full_path = true,
+      firefox_profile_name = nil,
+    },
+    symbols = {
+      sources = { "emoji", "kaomoji", "gitmoji", "latex", "math" },
+    },
+  },
 })
 
+tele.load_extension("dap")
 tele.load_extension("fzf")
+tele.load_extension('aerial')
 tele.load_extension("packer")
 tele.load_extension("luasnip")
 tele.load_extension("bookmarks")
+tele.load_extension("node_modules")
