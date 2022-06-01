fde37aed939ef202e688047fd2c2ad98afebdfc1
gingjin <gingjin@163.com>
2022-06-01 16:13:06 +0800

    add colorscheme.lua

---
+++ dot_config/nvim/lua/colorscheme.lua
@@ -0,0 +1,9 @@
+-- sonokai espresso
+--
+local g = vim.g
+g.sonokai_style = "espresso"
+g.sonokai_transparent_background = 0
+g.sonokai_menu_selection_background = "red"
+g.sonokai_better_performance = 1
+
+vim.cmd("colorscheme sonokai")
