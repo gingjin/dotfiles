32153bab9f40940a2ae6c47265dd4626c3f50110
gingjin <gingjin@163.com>
2022-06-01 15:51:13 +0800

    update toggleterm.lua

--- dot_config/nvim/lua/conf/toggleterm.lua
+++ dot_config/nvim/lua/conf/toggleterm.lua
@@ -1,24 +1,24 @@
 -- nvim toggleterm
 --
 require("toggleterm").setup({
-	size = 10,
-	open_mapping = [[<A-t>]],
-	shade_terminals = false,
-	start_in_insert = true,
-	insert_mappings = true,
-	persist_size = true,
-	close_on_exit = true,
-	shell = vim.o.shell,
-	-- direction = "horizontal",
-	direction = "float",
-	float_opts = {
-		border = "double",
-		-- winblend = 3,
-	},
-	highlights = {
-		Normal = {
-			guibg = "NONE",
-			ctermbg = "NONE",
-		},
-	},
+  size = 10,
+  open_mapping = [[<A-t>]],
+  shade_terminals = false,
+  start_in_insert = true,
+  insert_mappings = true,
+  persist_size = true,
+  close_on_exit = true,
+  shell = vim.o.shell,
+  -- direction = "horizontal",
+  direction = "float",
+  float_opts = {
+    border = "double",
+    -- winblend = 3,
+  },
+  highlights = {
+    Normal = {
+      guibg = "NONE",
+      ctermbg = "NONE",
+    },
+  },
 })
