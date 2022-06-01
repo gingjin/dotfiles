12220b9e005b28feb09568d8804ff1474727be75
gingjin <gingjin@163.com>
2022-06-01 15:47:31 +0800

    update format.lua

--- dot_config/nvim/lua/conf/format.lua
+++ dot_config/nvim/lua/conf/format.lua
@@ -1,7 +1,8 @@
 -- nvim neoformat
 --
 local g = vim.g
-g.neoformat_basic_format_trim = 1
-g.neoformat_basic_format_retab = 1
+g.neoformat_try_formatprg = 1
 g.neoformat_basic_format_align = 1
+g.neoformat_basic_format_retab = 1
+g.neoformat_basic_format_trim = 1
 g.neoformat_run_all_formatters = 1
