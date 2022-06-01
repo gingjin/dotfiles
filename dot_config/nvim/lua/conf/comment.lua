39268cb1de15e759e198bc2735420ff9efa9118a
gingjin <gingjin@163.com>
2022-06-01 15:47:03 +0800

    update comment.lua

--- dot_config/nvim/lua/conf/comment.lua
+++ dot_config/nvim/lua/conf/comment.lua
@@ -1,3 +1,41 @@
--- nvim comment
+-- nvim Comment
 --
-require("Comment").setup()
+require("Comment").setup({
+  padding = true,
+  sticky = true,
+  ignore = nil,
+  toggler = {
+    line = "gcc",
+    block = "gbc",
+  },
+  opleader = {
+    line = "gc",
+    block = "gb",
+  },
+  extra = {
+    above = "gcO",
+    below = "gco",
+    eol = "gcA",
+  },
+  mappings = {
+    basic = true,
+    extra = true,
+    extended = true,
+  },
+  pre_hook = function(ctx)
+    local U = require("Comment.utils")
+
+    local location = nil
+    if ctx.ctype == U.ctype.block then
+      location = require("ts_context_commentstring.utils").get_cursor_location()
+    elseif ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
+      location = require("ts_context_commentstring.utils").get_visual_start_location()
+    end
+
+    return require("ts_context_commentstring.internal").calculate_commentstring({
+      key = ctx.ctype == U.ctype.line and "__default" or "__multiline",
+      location = location,
+    })
+  end,
+  post_hook = nil,
+})
