83cc7e5fffdaca42af34bbc906f0fecca41cd7d9
gingjin <gingjin@163.com>
2022-06-01 15:51:45 +0800

    update treesitter.lua

--- dot_config/nvim/lua/conf/treesitter.lua
+++ dot_config/nvim/lua/conf/treesitter.lua
@@ -1,46 +1,55 @@
 -- nvim treesitter
 --
+
+-- use git
+require("nvim-treesitter.install").prefer_git = true
+
+-- use curl and proxy
+-- require("nvim-treesitter.install").command_extra_args = {
+--  curl = { "--proxy", "http://127.0.0.1:7890" },
+-- }
+
 require("nvim-treesitter.configs").setup({
-	-- ensure_installed = "all",
-	ensure_installed = {
-		"lua",
-		"c",
-		"cpp",
-		"python",
-		"html",
-		"css",
-		"json",
-		"markdown",
-		"bash",
-		"go",
-		"help",
-		"java",
-		"javascript",
-		"json5",
-		"vim",
-		"yaml",
-	},
-	ignore_install = {},
-	sync_install = false,
-	highlight = {
-		enable = true,
-		disable = { "latex" },
-		additional_vim_regex_highlighting = false,
-	},
-	indent = { enable = true },
-	rainbow = {
-		enable = true,
-		extended_mode = true,
-		max_file_lines = nil,
-	},
-	incremental_selection = {
-		enable = true,
-		keymaps = {
-			init_selection = "<CR>",
-			node_incremental = "<CR>",
-			node_decremental = "<BS>",
-			scope_incremental = "<TAB>",
-		},
-	},
-	autotag = { enable = true },
+  ensure_installed = "all",
+  sync_install = false,
+  ignore_install = { "phpdoc" },
+  highlight = {
+    enable = true,
+    disable = { "latex" },
+    additional_vim_regex_highlighting = false,
+  },
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = "<CR>",
+      node_incremental = "<CR>",
+      node_decremental = "<BS>",
+      scope_incremental = "<TAB>",
+    },
+  },
+  indent = { enable = true },
+  autotag = { enable = true },
+  rainbow = {
+    enable = true,
+    extended_mode = true,
+    max_file_lines = nil,
+  },
+  context_commentstring = {
+    enable = true,
+    enable_autocmd = false,
+    config = {
+      css = "// %s",
+      javascript = {
+        __default = "// %s",
+        jsx_element = "{/* %s */}",
+        jsx_fragment = "{/* %s */}",
+        jsx_attribute = "// %s",
+        comment = "// %s",
+      },
+      typescript = {
+        __default = "// %s",
+        __multiline = "/* %s */",
+      },
+    },
+  },
 })
