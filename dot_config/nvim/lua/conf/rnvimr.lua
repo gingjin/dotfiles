f385d85b9160cf8585ee3fa8e5aa2e92a67c06db
gingjin <gingjin@163.com>
2022-06-01 16:08:27 +0800

    update rnvimr.lua

--- dot_config/nvim/lua/conf/rnvimr.lua
+++ dot_config/nvim/lua/conf/rnvimr.lua
@@ -4,6 +4,7 @@ local g = vim.g
 g.rnvimr_enable_ex = 0
 g.rnvimr_enable_picker = 1
 g.rnvimr_hide_gitignore = 1
+g.rnvimr_draw_border = 1
 g.rnvimr_border_attr = { fg = 14, bg = -1 }
 g.rnvimr_enable_bw = 1
 
