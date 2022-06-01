cf112063e2f3eca73f7842ddbe0dd2eb7c04d963
gingjin <gingjin@163.com>
2022-06-01 15:46:17 +0800

    add lsp_/servers.lua

---
+++ dot_config/nvim/lua/conf/lsp_/servers.lua
@@ -0,0 +1,26 @@
+local M = {}
+M.servers = function()
+  return {
+    "bashls",
+    "clangd",
+    "cssls",
+    "gopls",
+    "html",
+    "intelephense",
+    "jdtls",
+    "jsonls",
+    "julials",
+    "lemminx",
+    "ltex",
+    "pyright",
+    "rust_analyzer",
+    "sorbet",
+    "sqlls",
+    "sumneko_lua",
+    "tsserver",
+    "vimls",
+    "vuels",
+    "yamlls",
+  }
+end
+return M
