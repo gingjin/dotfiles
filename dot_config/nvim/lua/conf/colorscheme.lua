1d931a829bd4ee4d174a1be7e5f3fdaf51e82d2e
gingjin <gingjin@163.com>
2022-06-01 15:57:55 +0800

    remove

--- dot_config/nvim/lua/conf/colorscheme.lua
+++
@@ -1,9 +0,0 @@
--- sonokai espresso
---
-local g = vim.g
-g.sonokai_style = "espresso"
--- g.sonokai_transparent_background = 1
-g.sonokai_menu_selection_background = "red"
-g.sonokai_better_performance = 1
-
-vim.cmd("colorscheme sonokai")
