20b7324a560e15baf568a1ab39749d20bd7b20d1
gingjin <gingjin@163.com>
2022-06-04 20:10:42 +0800

    update lsp_/servers.lua

--- dot_config/nvim/lua/conf/lsp_/servers.lua
+++ dot_config/nvim/lua/conf/lsp_/servers.lua
@@ -1,26 +1,24 @@
 local M = {}
-M.servers = function()
-  return {
-    "bashls",
-    "clangd",
-    "cssls",
-    "gopls",
-    "html",
-    "intelephense",
-    "jdtls",
-    "jsonls",
-    "julials",
-    "lemminx",
-    "ltex",
-    "pyright",
-    "rust_analyzer",
-    "sorbet",
-    "sqlls",
-    "sumneko_lua",
-    "tsserver",
-    "vimls",
-    "vuels",
-    "yamlls",
-  }
-end
+M.servers = {
+  "bashls",
+  "clangd",
+  "cssls",
+  "gopls",
+  "html",
+  "intelephense",
+  "jdtls",
+  "jsonls",
+  "julials",
+  "lemminx",
+  "ltex",
+  "pyright",
+  "rust_analyzer",
+  "sorbet",
+  "sqlls",
+  "sumneko_lua",
+  "tsserver",
+  "vimls",
+  "vuels",
+  "yamlls",
+}
 return M
