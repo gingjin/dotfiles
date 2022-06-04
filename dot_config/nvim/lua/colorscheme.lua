5bd4deda16f3184c8066bca5adc68b8eb753fcf8
gingjin <gingjin@163.com>
2022-06-04 20:13:03 +0800

    update colorscheme

--- dot_config/nvim/lua/colorscheme.lua
+++ dot_config/nvim/lua/colorscheme.lua
@@ -2,8 +2,10 @@
 --
 local g = vim.g
 g.sonokai_style = "espresso"
+g.sonokai_enable_italic = 1
 g.sonokai_transparent_background = 0
 g.sonokai_menu_selection_background = "red"
+g.sonokai_diagnostic_text_highlight = 1
 g.sonokai_better_performance = 1
 
 vim.cmd("colorscheme sonokai")
