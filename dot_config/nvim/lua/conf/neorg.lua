69a845e8516fa5a91f5d00c5e054692f693cc247
gingjin <gingjin@163.com>
2022-06-04 20:12:19 +0800

    update neorg

--- dot_config/nvim/lua/conf/neorg.lua
+++ dot_config/nvim/lua/conf/neorg.lua
@@ -7,26 +7,24 @@ require("neorg").setup({
     ["core.norg.journal"] = {},
     ["core.norg.concealer"] = {},
     ["core.norg.manoeuvre"] = {},
+    ["core.norg.completion"] = { config = { engine = "nvim-cmp" } },
     ["core.norg.dirman"] = {
       config = {
         workspaces = {
           work = "~/notes/work",
-          home = "~/notes/home",
         },
       },
     },
-    ["core.gtd.base"] = {
-      config = {
-        workspace = "work",
-      }
-    },
+    ["core.gtd.base"] = { config = { workspace = "work" } },
     ["core.gtd.ui"] = {},
     ["core.gtd.helpers"] = {},
     ["core.gtd.queries"] = {},
+    ["core.presenter"] = { config = { zen_mode = "zen-mode" } },
+    ["core.scanner"] = {},
     ["core.export"] = {},
     ["core.export.markdown"] = {},
     ["core.integrations.nvim-cmp"] = {},
-    ["core.scanner"] = {},
+    ["core.neorgcmd.commands.module.load"] = {},
     ["core.integrations.telescope"] = {},
     ["external.kanban"] = {},
   },
