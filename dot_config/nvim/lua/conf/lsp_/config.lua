f0302142ad9a912ce7eedbb9a0bf4add2fbdbbe9
gingjin <gingjin@163.com>
2022-06-04 20:10:01 +0800

    update lsp_/config.lua

--- dot_config/nvim/lua/conf/lsp_/config.lua
+++ dot_config/nvim/lua/conf/lsp_/config.lua
@@ -1,24 +1,47 @@
 -- nvim lspconfig
 --
-local on_attach = function(client, bufnr)
+vim.diagnostic.config({
+  virtual_text = {
+    spacing = 2,
+    prefix = "",
+  },
+  float = {
+    border = "double",
+  },
+  signs = true,
+  underline = true,
+  update_in_insert = false,
+  severity_sort = true,
+})
+
+local signs = { Error = "", Warn = "", Hint = "", Info = "" }
+for type, icon in pairs(signs) do
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+end
+
+local on_attach = function(_, bufnr)
   local function bufmap(...)
     vim.api.nvim_buf_set_keymap(bufnr, ...)
   end
-  require("keymaps").lsp(bufmap)
 
-  require("aerial").on_attach(client, bufnr)
+  require("keymaps").lsp(bufmap)
 end
 
 local handlers = {
-  ["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
-    underline = true,
-    virtual_text = { spacing = 2, prefix = "" },
-  }),
+  ["textDocument/hover"] =  vim.lsp.with(
+    vim.lsp.handlers.hover, { border = "double" }
+  ),
+  ["textDocument/signatureHelp"] =  vim.lsp.with(
+    vim.lsp.handlers.signature_help, { border = "double" }
+  ),
 }
 
-local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require("cmp_nvim_lsp").update_capabilities(
+  vim.lsp.protocol.make_client_capabilities()
+)
 
-for _, lsp in ipairs(require("conf.lsp_.servers").servers()) do
+for _, lsp in ipairs(require("conf.lsp_.servers").servers) do
   require("lspconfig")[lsp].setup({
     handlers = handlers,
     on_attach = on_attach,
