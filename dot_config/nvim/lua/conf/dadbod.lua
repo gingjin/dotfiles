15efeac74959758842fe47a0ce947244a8a0f5fe
gingjin <gingjin@163.com>
2022-06-05 00:38:20 +0800

    update dadbod

--- dot_config/nvim/lua/conf/dadbod.lua
+++ dot_config/nvim/lua/conf/dadbod.lua
@@ -1,7 +1,18 @@
 -- dadbod
 --
 local g = vim.g
-g.db_ui_use_nerd_fonts = 1
-g.db_ui_auto_execute_table_helpers = 1
-g.db_ui_show_help = 0
 g.db_ui_winwidth = 30
+g.db_ui_win_position = "left"
+g.db_ui_show_database_icon = 1
+g.db_ui_use_nerd_fonts = 1
+g.db_ui_show_help = 0
+g.db_ui_force_echo_notifications = 1
+g.db_ui_auto_execute_table_helpers = 1
+
+g.completion_chain_complete_list = {
+  ["sql"] = {
+    ["complete_items"] = { "vim-dadbod-completion" },
+  },
+}
+g.completion_matching_strategy_list = { "exact", "substring" }
+g.completion_matching_ignore_case = 1
