b61a5a73a7dfca2705fc56ba03cf92bbb70058e7
gingjin <gingjin@163.com>
2022-06-05 17:55:49 +0800

    add tasks.lua

---
+++ dot_config/nvim/lua/conf/tasks.lua
@@ -0,0 +1,10 @@
+-- asynctasks
+--
+local g = vim.g
+g.asyncrun_open = 10
+g.asynctasks_term_rows = 10
+g.asynctasks_term_cols = 80
+g.asynctasks_config_name = "tasks.ini"
+g.asynctasks_extra_config = {
+  "~/.config/nvim/tasks.ini",
+}
