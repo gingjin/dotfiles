1ae5302413bddc7b180f3f14e972cbaf742b8d12
gingjin <gingjin@163.com>
2022-12-12 17:43:16 +0800

    update options

--- dot_config/nvim/lua/options.lua
+++ dot_config/nvim/lua/options.lua
@@ -4,21 +4,18 @@ cmd("filetype plugin indent on")
 local g = vim.g
 g.mapleader = " "
 g.maplocalleader = " "
-g.python3_host_prog = "~/.pyenv/versions/py3.7/bin/python"
+g.python3_host_prog = "~/.pyenv/versions/py37env/bin/python"
 
 local opt = vim.opt
-opt.autoread = true
 opt.autowrite = true
-opt.clipboard = "unnamedplus"
+opt.clipboard:append("unnamedplus")
 opt.completeopt = "menu,menuone,noselect"
 opt.cursorline = true
 opt.encoding = "UTF-8"
 opt.expandtab = true
 opt.fileencodings = "ucs-bom,utf-8,gbk"
 opt.foldenable = false
-opt.foldexpr = "nvim_treesitter#foldexpr()"
 opt.foldlevel = 99
-opt.foldmethod = "expr"
 opt.ignorecase = true
 opt.mouse = "a"
 opt.number = true
