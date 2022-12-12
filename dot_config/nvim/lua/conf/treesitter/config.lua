66cd544270e2f0b243b8c285371d8a2de7df2a0b
gingjin <gingjin@163.com>
2022-12-12 22:29:30 +0800

    update treesitter

--- dot_config/nvim/lua/conf/treesitter/config.lua
+++ dot_config/nvim/lua/conf/treesitter/config.lua
@@ -7,7 +7,7 @@ require("nvim-treesitter.configs").setup({
   highlight = {
     enable = true,
     disable = { "latex" },
-    additional_vim_regex_highlighting = false,
+    additional_vim_regex_highlighting = {},
   },
   incremental_selection = {
     enable = true,
@@ -20,11 +20,7 @@ require("nvim-treesitter.configs").setup({
   },
   indent = { enable = true },
   autotag = { enable = true },
-  rainbow = {
-    enable = true,
-    extended_mode = true,
-    max_file_lines = nil,
-  },
+  rainbow = { enable = true, extended_mode = true, max_file_lines = nil, },
   context_commentstring = {
     enable = true,
     enable_autocmd = false,
@@ -37,56 +33,29 @@ require("nvim-treesitter.configs").setup({
         jsx_attribute = "// %s",
         comment = "// %s",
       },
-      typescript = {
-        __default = "// %s",
-        __multiline = "/* %s */",
-      },
+      typescript = { __default = "// %s", __multiline = "/* %s */", },
     },
   },
   textobjects = {
     swap = {
       enable = true,
-      swap_next = {
-        ["<leader>a"] = "@parameter.inner",
-      },
-      swap_previous = {
-        ["<leader>A"] = "@parameter.inner",
-      },
+      swap_next = { ["<leader>a"] = "@parameter.inner", },
+      swap_previous = { ["<leader>A"] = "@parameter.inner", },
     },
     select = {
       enable = true,
-      -- Automatically jump forward to textobj, similar to targets.vim
       lookahead = true,
       keymaps = {
-        -- You can use the capture groups defined in textobjects.scm
         ["af"] = "@function.outer",
         ["if"] = "@function.inner",
         ["ac"] = "@class.outer",
-        -- You can optionally set descriptions to the mappings (used in the desc parameter of
-        -- nvim_buf_set_keymap) which plugins like which-key display
         ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
       },
-      -- You can choose the select mode (default is charwise 'v')
-      --
-      -- Can also be a function which gets passed a table with the keys
-      -- * query_string: eg '@function.inner'
-      -- * method: eg 'v' or 'o'
-      -- and should return the mode ('v', 'V', or '<c-v>') or a table
-      -- mapping query_strings to modes.
       selection_modes = {
         ['@parameter.outer'] = 'v', -- charwise
         ['@function.outer'] = 'V', -- linewise
         ['@class.outer'] = '<c-v>', -- blockwise
       },
-      -- If you set this to `true` (default is `false`) then any textobject is
-      -- extended to include preceding or succeeding whitespace. Succeeding
-      -- whitespace has priority in order to act similarly to eg the built-in
-      -- `ap`.
-      --
-      -- Can also be a function which gets passed a table with the keys
-      -- * query_string: eg '@function.inner'
-      -- * selection_mode: eg 'v'
-      -- and should return true of false
       include_surrounding_whitespace = true,
     },
   },
