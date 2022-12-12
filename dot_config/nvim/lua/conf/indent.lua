23fc40eb2544b737e15b50bb5df2aad1be62c2f6
gingjin <gingjin@163.com>
2022-12-12 17:28:20 +0800

    update indent.lua

--- dot_config/nvim/lua/conf/indent.lua
+++ dot_config/nvim/lua/conf/indent.lua
@@ -1,25 +1,13 @@
 -- nvim indent-blankline
 --
-local cmd = vim.cmd
-cmd("highlight IndentBlanklineIndent1 guifg=#E06C75 gui=nocombine")
-cmd("highlight IndentBlanklineIndent2 guifg=#E5C07B gui=nocombine")
-cmd("highlight IndentBlanklineIndent3 guifg=#98C379 gui=nocombine")
-cmd("highlight IndentBlanklineIndent4 guifg=#56B6C2 gui=nocombine")
-cmd("highlight IndentBlanklineIndent5 guifg=#61AFEF gui=nocombine")
-cmd("highlight IndentBlanklineIndent6 guifg=#C678DD gui=nocombine")
+local opt = vim.opt
+opt.list = true
+opt.listchars = { tab = "⇝ ", }
 
 require("indent_blankline").setup({
-  char = "⎸",
-  show_current_context = false,
+  char = "▏", --⎸┆ ¦
+  show_current_context = true,
   show_current_context_start = false,
-  show_first_indent_level = true,
+  show_first_indent_level = false,
   show_trailing_blankline_indent = false,
-  char_highlight_list = {
-    "IndentBlanklineIndent1",
-    "IndentBlanklineIndent2",
-    "IndentBlanklineIndent3",
-    "IndentBlanklineIndent4",
-    "IndentBlanklineIndent5",
-    "IndentBlanklineIndent6",
-  },
 })
