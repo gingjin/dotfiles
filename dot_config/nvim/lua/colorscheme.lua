bc325870fda2b43bacc43e94c1d0a6402bafa7f7
gingjin <gingjin@163.com>
2022-12-13 19:57:12 +0800

    update colorscheme

--- dot_config/nvim/lua/colorscheme.lua
+++ dot_config/nvim/lua/colorscheme.lua
@@ -8,5 +8,5 @@ g.gruvbox_material_transparent_background = 1
 g.gruvbox_material_sign_column_background = "none"
 g.gruvbox_material_ui_contrast = "high"
 g.gruvbox_material_diagnostic_virtual_text = "colored"
-g.gruvbox_material_better_performance = 1
+g.gruvbox_material_dim_inactive_windows = 1
 vim.cmd("colorscheme gruvbox-material")
