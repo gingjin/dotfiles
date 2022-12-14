d1cab5854d0c9ed06a9c1a1049bf5f5c1bf84a54
gingjin <gingjin@163.com>
2022-12-14 15:39:25 +0800

    update outline for lspsaga

--- dot_config/nvim/lua/conf/lsp/ui.lua
+++ dot_config/nvim/lua/conf/lsp/ui.lua
@@ -5,7 +5,6 @@ saga.init_lsp_saga({
   border_style = "double",
   move_in_saga = { prev = "<C-p>", next = "<C-n>" },
   diagnostic_header = { " ", " ", " ", "ﴞ " },
-  --show_diagnostic_source = true,
   code_action_icon = "",
   code_action_num_shortcut = true,
   finder_icons = { def = ' ', ref = '諭', link = ' ', },
@@ -22,5 +21,14 @@ saga.init_lsp_saga({
   code_action_keys = { quit = "q", exec = "<CR>", },
   rename_action_quit = "<C-c>",
   rename_in_select = true,
-  --definition_preview_icon = "  ",
+  show_outline = {
+    win_position = 'right',
+    win_with = '',
+    win_width = 30,
+    auto_enter = true,
+    auto_preview = true,
+    virt_text = '┃',
+    jump_key = 'o',
+    auto_refresh = true,
+  },
 })
