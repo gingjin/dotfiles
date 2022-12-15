73f1e04bf09eec04766abe3f7a1af005b05b0513
gingjin <gingjin@163.com>
2022-12-15 11:10:43 +0800

    update autopairs

--- dot_config/nvim/lua/conf/pairs/config.lua
+++ dot_config/nvim/lua/conf/pairs/config.lua
@@ -18,23 +18,9 @@ npairs.setup({
 
 local cmp = require("cmp")
 local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-local handlers = require('nvim-autopairs.completion.handlers')
 cmp.event:on(
   "confirm_done",
-  cmp_autopairs.on_confirm_done({
-    filetypes = {
-      ["*"] = {
-        ["("] = {
-          kind = {
-            cmp.lsp.CompletionItemKind.Function,
-            cmp.lsp.CompletionItemKind.Method,
-          },
-          handler = handlers["*"]
-        }
-      },
-      tex = false
-    }
-  })
+  cmp_autopairs.on_confirm_done()
 )
 
 local Rule = require("nvim-autopairs.rule")
