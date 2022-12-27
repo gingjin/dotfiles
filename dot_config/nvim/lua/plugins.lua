b7a61c4fe0407a5ae4bee31bcd01815c87178c2e
gingjin <gingjin@163.com>
2022-12-27 17:49:38 +0800

    update plugins

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -17,7 +17,19 @@ packer.init({
   },
 })
 
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath("data") .. '/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
 packer.startup(function(use)
+  use({ "wbthomason/packer.nvim" })
   use({ "sainnhe/gruvbox-material" })
 
   use({ "jghauser/mkdir.nvim" })
@@ -115,32 +127,15 @@ packer.startup(function(use)
   use({ "iamcco/markdown-preview.nvim", config = function() require("conf.mkdp.init") end,
     run = "cd app && npm install", ft = { "markdown" },
     setup = function() vim.g.mkdp_filetypes = { "markdown" } end, })
+
+  if ensure_packer() then
+    require("packer").sync()
+  end
 end)
 
-local cmd = vim.api.nvim_create_autocmd
-local augroup = vim.api.nvim_create_augroup
-local Packer = augroup("Packer", { clear = true })
-cmd({
-  group = Packer,
-  pattern = "plugins.lua",
-  command = "source <afile> | PackerCompile",
-}, "BufWritePost")
-
--- local ensure_packer = function()
---   local fn = vim.fn
---   local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
---   if fn.empty(fn.glob(install_path)) > 0 then
---     fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
---     vim.cmd [[packadd packer.nvim]]
---     return true
---   end
---   return false
--- end
--- local packer = require("packer")
--- packer.startup(function(use)
---   use({ "wbthomason/packer.nvim" })
---
---   if ensure_packer() then
---     require('packer').sync()
---   end
--- end)
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerCompile
+  augroup end
+]])
