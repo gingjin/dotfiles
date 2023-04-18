cb09611cafcda2e479a22064bc6123bddf379420
gingjin <gingin@163.com>
2023-04-18 15:52:57 +0800

    remove old files

--- dot_config/nvim/lua/plugins/lazygit/init.lua
+++
@@ -1,5 +0,0 @@
-local opts = { noremap = true, silent = true }
-local G = require("G")
-G.map({
-  { "n", "<leader>gg", ":LazyGit<CR>", opts },
-})
--- dot_config/nvim/lua/plugins/lint/init.lua
+++
@@ -1,6 +0,0 @@
-require("lint").linters_by_ft = {
-  cpp = { "cpplint" },
-  markdown = { "markdownlint" },
-}
-
-vim.cmd("au TextChanged * lua require('lint').try_lint()")
