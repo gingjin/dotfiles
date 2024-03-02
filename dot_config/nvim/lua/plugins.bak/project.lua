973c294d53dcc3555de680b98ce994e74607727a
gingjin <gingjin@163.com>
2024-03-02 11:32:53 +0800

    update nvim

---
+++ dot_config/nvim/lua/plugins.bak/project.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "ahmedkhalf/project.nvim",
+    opts = {
+      manual_mode = true,
+      detection_methods = { "lsp", "pattern" },
+      patterns = { ".git", "_darcs", ".hg", ".bzr", ".svn", "Makefile", "package.json" },
+      ignore_lsp = {},
+      exclude_dirs = {},
+      show_hidden = true,
+      silent_chdir = true,
+      scope_chdir = "global",
+      datapath = vim.fn.stdpath("data"),
+    },
+    config = function(_, opts)
+      require("project_nvim").setup(opts)
+    end,
+  },
+}
